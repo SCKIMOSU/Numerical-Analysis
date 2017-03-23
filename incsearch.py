7270f0e12d0ef2ca079d447b66b09f55c0d30dc7
SCKIMOSU <sckim7@kookmin.ac.kr>
2017-03-23 11:36:13 +0900

    Update incsearch.py

--- incsearch.py
+++ incsearch.py
@@ -6,7 +6,7 @@ func1=lambda x1: np.sin(np.dot(10.0,x1))+np.cos(np.dot(3.0, x1))
 f1=func1(x1)
 plt.figure(1)
 plt.plot(x1,f1, 'ro-')
-#plt.grid()
+plt.grid()
 #plt.show()
 
 x2=np.linspace(3, 6, 100)
@@ -14,7 +14,7 @@ func2=lambda x2: np.sin(np.dot(10.0,x2))+np.cos(np.dot(3.0, x2))
 f2=func1(x2)
 plt.figure(2)
 plt.plot(x2,f2, 'bd-')
-#plt.grid()
+plt.grid()
 #plt.show()
 
 def incsearch(func, xmin, xmax, ns):
@@ -47,6 +47,5 @@ nb1, xb1=incsearch(func, 3, 6, 100)
 print('number of brackets= ', nb1)
 print('root interval=', xb1)
 
-# print graphs 
-plt.grid()
+
 plt.show()
