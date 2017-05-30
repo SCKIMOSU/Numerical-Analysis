c3cafaad3235fbec9e6826e1e047b368f479beaa
sckimosu <sckim7@kookmin.ac.kr>
2017-05-30 13:10:38 +0900

    Add trapezoid.py

--- trapezoid.py
+++ trapezoid.py
@@ -12,10 +12,36 @@ real=re[0]
 plt.figure(1)
 plt.plot(x, f(x), 'ro-')
 
+#  n=1 segment
 I=(0.8-0)*(f(0.8)+f(0))/2
+# 0.1728000000000225
+
 trap=np.ones(80)*(f(0.8)+f(0))/2
 
 plt.plot(x, trap, 'b*-')
 
 error=(real-I)/real*100
 
+#  n=2 segments
+#
+I1=(0.8-0)*(f(0)+2*f(0.4)+f(0.8))/(2*2)
+
+trap1=np.ones(40)*(f(0.4)+f(0))/2
+
+plt.plot(x[0:39], trap1, 'c*-')
+
+trap2=np.ones(40)*(f(0.4)+f(0.8))/2
+plt.plot(x[40:80], trap2, 'c*-')
+
+# I1=1.0688000000000115
+
+error1=(real-I1)/real*100
+
+# 34.850455136540162 %
+
+# n=4 segments
+I2=(0.8-0)*(f(0)+2*(f(0.2)+f(0.4)+f(0.6))+f(0.8))/(4*2)
+# I2= 1.484800000000001
+
+error2=(real-I2)/real*100
+# error2= 9.4928478543561035
