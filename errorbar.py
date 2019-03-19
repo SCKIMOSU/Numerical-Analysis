10eeb4eb9cc2e9021465ae883623f8f3038febb7
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-03-19 13:31:43 +0900

    Create errorbar.py

---
+++ errorbar.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# example data
+x = np.arange(0.1, 4, 0.5)
+y = np.exp(-x)
+
+# example error bar values that vary with x-position
+error = 0.1 + 0.2 * x
+
+fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True)
+ax0.errorbar(x, y, yerr=error, fmt='-o')
+ax0.set_title('variable, symmetric error')
+
+# error bar values w/ different -/+ errors that
+# also vary with the x-position
+lower_error = 0.4 * error
+upper_error = error
+asymmetric_error = [lower_error, upper_error]
+
+ax1.errorbar(x, y, xerr=asymmetric_error, fmt='o')
+ax1.set_title('variable, asymmetric error')
+ax1.set_yscale('log')
+plt.show()
