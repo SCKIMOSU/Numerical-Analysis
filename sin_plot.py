64f7942a47d214427deded1454ae0101e94ee72f
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-03-19 14:05:56 +0900

    Create sin_plot.py

---
+++ sin_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+t=np.arange(-2*np.pi, 2*np.pi, 0.5)
+y1=np.sin(t)
+y2=np.cos(t)
+plt.subplot(2, 1, 1)
+plt.plot(t, y1, 'ko-')
+plt.title('2 subplots')
+plt.xlabel('time (s)')
+plt.ylabel('Sine')
+plt.grid(True)
+plt.subplot(2, 1, 2)
+plt.plot(t, y2, 'r.-')
+plt.xlabel('time (s)')
+plt.ylabel('Cosine')
+plt.grid(True)
+plt.show()
