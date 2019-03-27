00091dafb5e4598329efdff80faef059f1daa3d9
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-03-27 16:11:42 +0900

    Update incremental1.py

--- incremental1.py
+++ incremental1.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 def incsearch(func, xmin, xmax, ns):
     x=np.linspace(xmin, xmax, ns)
     f=func(x)
@@ -13,17 +15,20 @@ def incsearch(func, xmin, xmax, ns):
     xb=xbt.reshape(nb, 2)
     return nb, xb
 
-xmin=3; xmax=6
-func=lambda x: np.sin(np.dot(10.0, x))+np.cos(np.dot(3.0, x))
-nb, xb=incsearch(func, 3, 6, 50)
-print('number of brackets= ', nb)
-print('root interval=', xb)
+def draw():
+    x = np.linspace(3, 6, 50)
+    func = lambda x: np.sin(np.dot(10.0, x)) + np.cos(np.dot(3.0, x))
+    f1 = func(x)
+    plt.figure(1)
+    plt.plot(x, f1, 'ro-')
+    plt.grid()
 
 
-import matplotlib.pyplot as plt
-x=np.linspace(3, 6, 50)
-func=lambda x: np.sin(np.dot(10.0, x))+np.cos(np.dot(3.0, x))
-f1=func(x)
-plt.figure(1)
-plt.plot(x,f1, 'ro-')
-plt.grid()
+if __name__ == '__main__':
+
+    xmin=3; xmax=6
+    func=lambda x: np.sin(np.dot(10.0, x))+np.cos(np.dot(3.0, x))
+    nb, xb=incsearch(func, 3, 6, 50)
+    draw()
+    print('number of brackets= ', nb)
+    print('root interval=', xb)
