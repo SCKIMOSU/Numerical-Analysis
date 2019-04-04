c7a2cb42b00d4ad5afa0a1f53ecc4868f8c3a194
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-04-04 14:04:43 +0900

    Update newton.py

--- newton.py
+++ newton.py
@@ -2,38 +2,37 @@ import numpy as np
 
 
 def sech(x):
-    return np.cosh(x)**(-1)
+    return np.cosh(x) ** (-1)
 
 
 def newton_raphson(func, dfunc, xr):
-    maxit=50
-    es=1.0e-5
-    iter=0
+    maxit = 50
+    es = 1.0e-5
+    iter = 0
 
-    while(1):
-        xrold=xr
-        xr=np.float(xr-func(xr)/dfunc(xr))
-        iter=iter+1
+    while (1):
+        xrold = xr
+        xr = np.float(xr - func(xr) / dfunc(xr))
+        iter = iter + 1
 
         if xr != 0:
-            ea=np.float(np.abs((np.float(xr)-np.float(xrold))/np.float(xr))*100)
-
+            ea = np.float(np.abs((np.float(xr) - np.float(xrold)) / np.float(xr)) * 100)
 
         if np.int(ea <= es) | np.int(iter >= maxit):
             break
-            
-        root=xr
-        fx=func(xr)
+
+        root = xr
+        fx = func(xr)
     return root, fx, ea, iter
 
+if __name__ == '__main__':
+    g = 9.81; cd = 0.25; v = 36; t = 4;
+    fp = lambda m: np.sqrt(g * m / cd) * np.tanh(np.sqrt(g * cd / m) * t) - v
+    dfp = lambda m: (1 / 2) * np.sqrt(g / (m * cd)) * np.tanh(np.sqrt(g * cd / m) * t) - g * t / (2 * m) * (
+    sech(np.sqrt(g * cd / m) * t)) ** 2
 
-g=9.81; cd=0.25; v=36; t=4;
-fp=lambda m: np.sqrt(g*m/cd)*np.tanh(np.sqrt(g*cd/m)*t)-v
-
-dfp=lambda m: (1/2)*np.sqrt(g/(m*cd))*np.tanh(np.sqrt(g*cd/m)*t)-g*t/(2*m)*(sech(np.sqrt(g*cd/m)*t))**2
-
-root, fx, ea, iter=newton_raphson(fp, dfp, 140)
-print('root weight= ', root)
-print('f(root weight, should be zero) =', fx)
-print('ea = should be less than 1.0e-4', ea)
-print('iter =', iter)
+    root, fx, ea, iter = newton_raphson(fp, dfp, 140)
+    print('root weight= ', root)
+    print('f(root weight, should be zero) =', fx)
+    print('ea = should be less than 1.0e-4', ea)
+    print('iter =', iter)
