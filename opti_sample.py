32db6b90d2ae69bf0901bed75dd2db833b88ffa3
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-04-15 21:34:32 +0900

    Create opti_sample.py

---
+++ opti_sample.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x1=np.linspace(0,10,50)
+x2=np.linspace(-10,10,50)
+y1=x1**3-9*x1**2+24*x1-7
+y2=x2**3-9*x2**2+24*x2-7
+plt.figure(1)
+plt.plot(x1, y1, 'ro-')
+plt.grid()
+plt.show()
+plt.figure(2)
+plt.plot(x2, y2, 'b*-')
+plt.grid()
+plt.show()
