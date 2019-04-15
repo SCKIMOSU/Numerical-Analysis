a5f437dc79d6eb422dae1f3d5bdca28d9af86971
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-04-15 21:38:53 +0900

    Update opti_sample.py

--- opti_sample.py
+++ opti_sample.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
 x1=np.linspace(0,10,50)
 x2=np.linspace(-10,10,50)
 y1=x1**3-9*x1**2+24*x1-7
@@ -12,3 +14,7 @@ plt.figure(2)
 plt.plot(x2, y2, 'b*-')
 plt.grid()
 plt.show()
+
+fx=lambda x: x**3-9*x**2+24*x-7
+x=fsolve(fx, 1)
+print("Real Root= ", x)
