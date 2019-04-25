257f69fd5057e05a18a7bb8a6309b853bdfdc713
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-04-25 11:43:11 +0900

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
 
 X = [1., 2., 3.]
 Y = [1., 2., 3.]
 m = len(X)
-
 W = tf.placeholder(tf.float32)
 
 #hypothesis = tf.mul(W, X)
@@ -11,7 +12,6 @@ hypothesis = W*X
 cost = tf.reduce_sum(tf.pow(hypothesis-Y, 2)) / m
 
 init = tf.initialize_all_variables()
-
 sess = tf.Session()
 sess.run(init)
 
@@ -22,20 +22,17 @@ W_val, cost_val = [], []
 for i in range(-30, 50):
     xPos = i*0.1                                    # x 좌표. -3에서 5까지 0.1씩 증가
     yPos = sess.run(cost, feed_dict={W: xPos})      # x 좌표에 따른 y 값
-
     print('{:3.1f}, {:3.1f}'.format(xPos, yPos))
-
     # 그래프에 표시할 데이터 누적. 단순히 리스트에 갯수를 늘려나감
     W_val.append(xPos)
     cost_val.append(yPos)
-
 sess.close()
-
 # ------------------------------------------ #
-
-import matplotlib.pyplot as plt
-
+print('size(W_val=)', np.size(W_val))
+print('W_val=', W_val)
+print('cost_val=', cost_val)
 plt.plot(W_val, cost_val, 'ro')
 plt.ylabel('Cost')
 plt.xlabel('W')
+plt.grid()
 plt.show()
