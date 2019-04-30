eaac04e37ed95dc5533eebcfb67134e7e7d14865
SCKIMOSU <sckim7@kookmin.ac.kr>
2019-04-30 14:17:38 +0900

    Update linear_envelope.py

--- linear_envelope.py
+++ linear_envelope.py
@@ -2,22 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def linear_enveope(x,y):
-
     Cost=[]
     Slope=[]
-    for i in np.linspace(-3, 5, 80):
+    for i in np.arange(-3, 5, 0.1): #np.linspace(-3, 5, 80):
         Ct=(sum((i * x - y) ** 2)) / np.size(x)
-
         Cost.append(Ct)
         Slope.append(i)
-
     return Cost, Slope
+
+def draw(Cost,Slope):
+    plt.plot(Slope, Cost, '*')
+    plt.show()
+    plt.grid()
+    plt.xlabel('Slope')
+    plt.ylabel('Cost')
+
 
 if __name__ == '__main__':
     x=np.array([1,2,3])
     y=np.array([1,2,3])
     Cost, Slope=linear_enveope(x,y)
-
-    plt.plot(Slope, Cost, '*')
-    plt.show()
-    plt.grid()
+    zipped=list(zip(Slope, Cost))
+    draw(Cost, Slope)
