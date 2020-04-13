01be2306b43d32df677f6dc011da0455222fb746
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-04-13 11:03:31 +0900

    Update newton.py

--- newton.py
+++ newton.py
@@ -7,7 +7,7 @@ def sech(x):
     return np.cosh(x) ** (-1)
 
 
-def newton_raphson(func, dfunc, xr):
+def newton_raphson(func, dfunc, xr, xt):
     maxit = 50
     es = 1.0e-5
     iter = 0
@@ -15,10 +15,12 @@ def newton_raphson(func, dfunc, xr):
     while (1):
         xrold = xr
         xr = np.float(xr - func(xr) / dfunc(xr))
+
         iter = iter + 1
 
         if xr != 0:
             ea = np.float(np.abs((np.float(xr) - np.float(xrold)) / np.float(xr)) * 100)
+            et= np.float(np.abs((np.float(xt) - np.float(xr)) / np.float(xt)) * 100)
 
         if np.int(ea <= es) | np.int(iter >= maxit):
             break
@@ -29,17 +31,19 @@ def newton_raphson(func, dfunc, xr):
 
 if __name__ == '__main__':
     g = 9.81; cd = 0.25; v = 36; t = 4
+    fm = lambda m: np.sqrt(9.81 * m / 0.25) * np.tanh(np.sqrt(9.81 * 0.25 / m) * 4) - 36
+    xt = fsolve(fm, 1)
+    print("Real Root= ", xt)
+
     fp = lambda m: np.sqrt(g * m / cd) * np.tanh(np.sqrt(g * cd / m) * t) - v
     dfp = lambda m: (1 / 2) * np.sqrt(g / (m * cd)) * np.tanh(np.sqrt(g * cd / m) * t) - g * t / (2 * m) * (
     sech(np.sqrt(g * cd / m) * t)) ** 2
 
-    root, fx, ea, iter = newton_raphson(fp, dfp, 140)
+    root, fx, ea, iter = newton_raphson(fp, dfp, 140, xt)
     print('root weight= ', root)
     print('f(root weight, should be zero) =', fx)
     print('ea = should be less than 1.0e-4', ea)
     print('iter =', iter)
 
-    fm = lambda m: np.sqrt(9.81 * m / 0.25) * np.tanh(np.sqrt(9.81 * 0.25 / m) * 4) - 36
-    m = fsolve(fm, 1)
-    print("Real Root= ", m)
+
 
