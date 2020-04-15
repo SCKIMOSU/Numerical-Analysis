f517f5727e3f4af8fb214de6ee9637e2ca3c7d67
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-04-15 21:59:27 +0900

    Create statistics.py

---
+++ statistics.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+n1=np.random.randn(10)
+u=np.random.uniform(-1, 1, 10)
+plt.plot(n1,'o:')
+plt.plot(u, '*:')
+
+
+n1=np.random.randn(10000)
+c1=np.size(np.where((n1>=-1) & (n1<=1) ))
+print('c1 = ', c1)
+n2=np.random.randn(10000)*2
+c2=np.size(np.where((n2>=-2) & (n2<=2) ))
+print('c2 = ', c2)
+n3=np.random.randn(10000)+2
+c3=np.size(np.where((n3>=1) & (n3<=3) ))
+print('c3 = ', c3)
+n4=2*np.random.randn(10000)+2
+c4=np.size(np.where((n4>=0) & (n4<=4) ))
+print('c4 = ', c4)
+
+plt.figure(2)
+plt.hist(n1, 100, normed=1)
+plt.hist(n2, 100, normed=1)
+plt.hist(n3, 100, normed=1)
+plt.hist(n4, 100, normed=1)
