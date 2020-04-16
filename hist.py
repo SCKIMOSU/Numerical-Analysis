a957afc9ad8c55428821bc08c0c7dfe204efa66b
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-04-16 10:16:14 +0900

    Update hist.py

--- hist.py
+++ hist.py
@@ -8,7 +8,7 @@ plt.hist(s,5)
 plt.figure(5)
 plt.hist(s,4, color='g')
 
-# bar() 히스토그램 그리기
+# plt.bar()히스토그램 그리기
 smin=s.min()
 smax=s.max()
 bin_width=(s.max()-s.min())/4
@@ -36,3 +36,15 @@ plt.bar(bins_left, bins_height, width = bin_width-0.05)
 plt.grid()
 plt.show()
 
+# plt.bar()와 plt.hist()를 이용한 히스토그램
+plt.figure(7)
+plt.subplot(2, 1, 1)
+plt.bar(bins_left, bins_height, bin_width)
+plt.title('Bar')
+plt.ylabel('Count')
+plt.grid()
+plt.subplot(2, 1, 2)
+plt.hist(s, bins=4, color='c', alpha=0.75)
+plt.title('Histrogram')
+plt.ylabel('Count')
+plt.grid()
