f49a282f176c80f125e84b782d1a7b59135cf7c1
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-05-10 12:50:19 +0900

    Update objective_function.py

--- objective_function.py
+++ objective_function.py
@@ -77,3 +77,153 @@ plt.colorbar(cp)
 plt.savefig('draw_contour.svg')
 plt.show()
 
+
+
+def f1d(x):
+    """f1(x)의 도함수"""
+    return 2 * (x - 2.0)
+
+
+xx = np.linspace(-1, 4, 100)
+
+plt.plot(xx, f1(xx), 'k-')
+
+# step size
+mu = 0.4
+
+# k = 0
+x = 0
+plt.plot(x, f1(x), 'go', markersize=10)
+#plt.show()
+plt.text(x + 0.1, f1(x) + 0.1, "1차 시도", fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("1차 시도: x_1 = {:.2f}, g_1 = {:.2f}".format(x, f1d(x)))
+
+# k = 1
+x = x - mu * f1d(x)
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x - 0.2, f1(x) + 0.4, "2차 시도" , fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("2차 시도: x_2 = {:.2f}, g_2 = {:.2f}".format(x, f1d(x)))
+
+# k = 2
+x = x - mu * f1d(x)
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x - 0.2, f1(x) - 0.7, "3차 시도", fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("3차 시도: x_3 = {:.2f}, g_3 = {:.2f}".format(x, f1d(x)))
+
+plt.xlabel("x")
+plt.ylabel("$f_1(x)$")
+plt.title("최대경사법을 사용한 1차함수의 최적화", fontdict=font3)
+plt.ylim(0, 10)
+plt.show()
+
+
+
+xx = np.linspace(-3, 8, 100)
+plt.figure(2)
+plt.plot(xx, f1(xx), 'k-')
+#plt.show()
+
+# step size (너무 큰 값!)
+mu = 1.1
+
+# k = 0
+x = 0
+
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x + 0.2, f1(x) + 0.1, "1차 시도", fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("1차 시도: x_1 = {:.2f}, g_1 = {:.2f}".format(x, f1d(x)))
+
+# k = 1
+x = x - mu * f1d(x)
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x + 0.2, f1(x) + 0.4, "2차 시도", fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("2차 시도: x_2 = {:.2f}, g_2 = {:.2f}".format(x, f1d(x)))
+
+# k = 2
+x = x - mu * f1d(x)
+plt.plot(x, f1(x), 'go', markersize=10)
+plt.text(x - 1.2, f1(x) - 0.7, "3차 시도", fontdict=font3)
+plt.plot(xx, f1d(x) * (xx - x) + f1(x), 'b--')
+print("3차 시도: x_3 = {:.2f}, g_3 = {:.2f}".format(x, f1d(x)))
+
+plt.ylim(0, 15)
+plt.xlabel("x")
+plt.ylabel("$f_1(x)$")
+plt.title("최대경사법을 사용한 1차함수의 최적화 (스텝 사이즈가 너무 큰 경우)", fontdict=font3)
+plt.show()
+
+def f2g(x, y):
+    """f2(x, y)의 도함수"""
+    return np.array((2.0 * (x - 1) - 400.0 * x * (y - x**2), 200.0 * (y - x**2)))
+
+#다음 그림에  x=−1,y−1 에서 시작하여
+# 최대경사법으로 최적점을 찾아나가는 과정을 그레디언트 벡터 화살표와 함께 보였다.
+
+xx = np.linspace(-4, 4, 800)
+yy = np.linspace(-3, 3, 600)
+X, Y = np.meshgrid(xx, yy)
+Z = f2(X, Y)
+
+levels = np.logspace(-1, 3, 10)
+
+plt.contourf(X, Y, Z, alpha=0.2, levels=levels)
+plt.contour(X, Y, Z, colors="green", levels=levels, zorder=0)
+plt.plot(1, 1, 'ro', markersize=10)
+#plt.show() # debug 시에만 사용할 것
+
+mu = 8e-4  # step size
+s = 0.95  # for arrowhead drawing
+
+x, y = -1, -1
+for i in range(5):
+    g = f2g(x, y)
+    plt.arrow(x, y, -s * mu * g[0], -s * mu * g[1],
+              head_width=0.04, head_length=0.04, fc='k', ec='k', lw=2)
+    x = x - mu * g[0]
+    y = y - mu * g[1]
+
+plt.xlim(-3, 3)
+plt.ylim(-2, 2)
+plt.xticks(np.linspace(-3, 3, 7))
+plt.yticks(np.linspace(-2, 2, 5))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("최대경사법을 사용한 2차함수의 최적화" , fontdict=font3)
+plt.show()
+
+xx = np.linspace(0, 4, 800)
+yy = np.linspace(0, 3, 600)
+X, Y = np.meshgrid(xx, yy)
+Z = f2(X, Y)
+
+levels = np.logspace(-1, 4, 20)
+
+plt.contourf(X, Y, Z, alpha=0.2, levels=levels)
+plt.contour(X, Y, Z, colors="green", levels=levels, zorder=0)
+plt.plot(1, 1, 'ro', markersize=10)
+
+mu = 1.8e-3  # 스텝 사이즈
+s = 0.95  # 화살표 크기
+
+x, y = 1.5, 1.5
+for i in range(15):
+    g = f2g(x, y)
+    plt.arrow(x, y, -s * mu * g[0], -s * mu * g[1],
+              head_width=0.04, head_length=0.04, fc='k', ec='k', lw=2)
+    x = x - mu * g[0]
+    y = y - mu * g[1]
+
+plt.xlim(0, 3)
+plt.ylim(0, 2)
+plt.xticks(np.linspace(0, 3, 4))
+plt.yticks(np.linspace(0, 2, 3))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("최대경사법을 사용한 2차함수의 최적화 (진동 현상)" , fontdict=font3)
+plt.show()
+
