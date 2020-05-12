05220c729857d8865baa9d0d43e2bab8ed25e553
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-05-12 14:01:35 +0900

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,450 @@
+# 리스트 5-1-(1)
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+
+# 데이터 생성 --------------------------------
+np.random.seed(seed=1) # 난수를 고정
+X_min = 4 # X의 하한(표시 용)
+X_max = 30 # X의 상한(표시 용)
+X_n = 16 # X의 상한(표시 용)
+X = 5 + 25 * np.random.rand(X_n)
+Prm_c = [170, 108, 0.2] # 생성 매개 변수
+T = Prm_c[0] - Prm_c[1] * np.exp(-Prm_c[2] * X) \
++ 4 * np.random.randn(X_n) # (A)
+'''''''''
+np.random.rand?
+Docstring:
+rand(d0, d1, ..., dn)
+Random values in a given shape.
+Create an array of the given shape and populate it with
+random samples from a uniform distribution
+over ``[0, 1)``.
+
+np.random.randn?
+Docstring:
+randn(d0, d1, ..., dn)
+Return a sample (or samples) from the "standard normal" distribution.
+
+'''
+np.savez('ch5_data.npz', X=X, X_min=X_min, X_max=X_max, X_n=X_n, T=T) # (B)
+
+# 리스트 5-1-(12)
+# 2차원 데이터 생성 --------------------------
+X0 = X
+X0_min = 5
+X0_max = 30
+np.random.seed(seed=1) # 난수를 고정
+X1 = 23 * (T / 100)**2 + 2 * np.random.randn(X_n)
+X1_min = 40
+X1_max = 75
+
+# 리스트 5-1-(5)
+# 데이터 그래프 ------------------------------
+plt.figure(figsize=(4, 4))
+plt.plot(X, T, marker='o', linestyle='None',
+         markeredgecolor='black', color='cornflowerblue')
+plt.xlim(X_min, X_max)
+plt.grid(True)
+plt.show()
+
+
+from mpl_toolkits.mplot3d import Axes3D
+# 평균 오차 함수 ------------------------------
+def mse_line(x, t, w):
+    y = w[0] * x + w[1]
+    mse = np.mean((y - t)**2)
+    return mse
+
+
+
+
+# 계산 --------------------------------------
+xn = 100 # 등고선 표시 해상도
+w0_range = [-25, 25]
+w1_range = [120, 170]
+x0 = np.linspace(w0_range[0], w0_range[1], xn)
+x1 = np.linspace(w1_range[0], w1_range[1], xn)
+xx0, xx1 = np.meshgrid(x0, x1)
+J = np.zeros((len(x0), len(x1)))
+for i0 in range(xn):
+    for i1 in range(xn):
+        J[i1, i0] = mse_line(X, T, (x0[i0], x1[i1]))
+
+# 표시 --------------------------------------
+plt.figure(figsize=(9.5, 4))
+plt.subplots_adjust(wspace=0.5)
+
+ax = plt.subplot(1, 2, 1, projection='3d')
+ax.plot_surface(xx0, xx1, J, rstride=10, cstride=10, alpha=0.3,
+                color='blue', edgecolor='black')
+ax.set_xticks([-20, 0, 20])
+ax.set_yticks([120, 140, 160])
+ax.view_init(20, -60)
+
+plt.subplot(1, 2, 2)
+cont = plt.contour(xx0, xx1, J, 30, colors='black',
+                   levels=[100, 1000, 10000, 100000])
+cont.clabel(fmt='%1.0f', fontsize=8)
+plt.grid(True)
+plt.show()
+
+
+# 리스트 5-1-(7)
+# 평균 제곱 오차의 기울기 ------------------------
+def dmse_line(x, t, w):
+    y = w[0] * x + w[1]
+    d_w0 = 2 * np.mean((y - t) * x)
+    d_w1 = 2 * np.mean(y - t)
+    return d_w0, d_w1
+
+
+# 리스트 5-1-(9)
+# 구배법 ------------------------------------
+def fit_line_num(x, t):
+    w_init = [10.0, 165.0] # 초기 매개 변수
+    alpha = 0.001 # 학습률
+    i_max = 100000 # 반복의 최대 수
+    eps = 0.1 # 반복을 종료 기울기의 절대 값의 한계
+    w_i = np.zeros([i_max, 2])
+    w_i[0, :] = w_init
+    for i in range(1, i_max):
+        dmse = dmse_line(x, t, w_i[i - 1])
+        w_i[i, 0] = w_i[i - 1, 0] - alpha * dmse[0]
+        w_i[i, 1] = w_i[i - 1, 1] - alpha * dmse[1]
+        if max(np.absolute(dmse)) < eps: # 종료판정, np.absolute는 절대치
+            break
+    w0 = w_i[i, 0]
+    w1 = w_i[i, 1]
+    w_i = w_i[:i, :]
+    return w0, w1, dmse, w_i
+
+
+# 메인 ------------------------------------
+plt.figure(figsize=(4, 4)) # MSE의 등고선 표시
+xn = 100 # 등고선 해상도
+w0_range = [-25, 25]
+w1_range = [120, 170]
+x0 = np.linspace(w0_range[0], w0_range[1], xn)
+x1 = np.linspace(w1_range[0], w1_range[1], xn)
+xx0, xx1 = np.meshgrid(x0, x1)
+J = np.zeros((len(x0), len(x1)))
+for i0 in range(xn):
+    for i1 in range(xn):
+        J[i1, i0] = mse_line(X, T, (x0[i0], x1[i1]))
+cont = plt.contour(xx0, xx1, J, 30, colors='black',
+                   levels=(100, 1000, 10000, 100000))
+cont.clabel(fmt='%1.0f', fontsize=8)
+plt.grid(True)
+# 구배법 호출
+W0, W1, dMSE, W_history = fit_line_num(X, T)
+# 결과보기
+print('반복 횟수 {0}'.format(W_history.shape[0]))
+print('W=[{0:.6f}, {1:.6f}]'.format(W0, W1))
+print('dMSE=[{0:.6f}, {1:.6f}]'.format(dMSE[0], dMSE[1]))
+print('MSE={0:.6f}'.format(mse_line(X, T, [W0, W1])))
+plt.plot(W_history[:, 0], W_history[:, 1], '.-',
+         color='gray', markersize=10, markeredgecolor='cornflowerblue')
+plt.show()
+
+
+# 리스트 5-1-(10)
+# 선 표시 ----------------------------------
+def show_line(w):
+    xb = np.linspace(X_min, X_max, 100)
+    y = w[0] * xb + w[1]
+    plt.plot(xb, y, color=(.5, .5, .5), linewidth=4)
+
+
+# 리스트 5-1-(11)
+# 해석해 ------------------------------------
+def fit_line(x, t):
+    mx = np.mean(x)
+    mt = np.mean(t)
+    mtx = np.mean(t * x)
+    mxx = np.mean(x * x)
+    w0 = (mtx - mt * mx) / (mxx - mx**2)
+    w1 = mt - w0 * mx
+    return np.array([w0, w1])
+
+
+
+
+if  __name__ == '__main__':  # 리스트 5-1-(8)
+    d_w = dmse_line(X, T, [10, 165])
+    print(np.round(d_w, 1))
+
+    # 메인 ------------------------------------
+    plt.figure(figsize=(4, 4))
+    W=np.array([W0, W1])
+    mse = mse_line(X, T, W)
+    print("w0={0:.3f}, w1={1:.3f}".format(W0, W1))
+    # mse = mse_line(X, T, W)
+    print("SD={0:.3f} cm".format(np.sqrt(mse)))
+    show_line(W)
+    plt.plot(X, T, marker='o', linestyle='None',
+             color='cornflowerblue', markeredgecolor='black')
+    plt.xlim(X_min, X_max)
+    plt.grid(True)
+    plt.show()
+
+    # 메인 ------------------------------------
+    W = fit_line(X, T)
+    print("w0={0:.3f}, w1={1:.3f}".format(W[0], W[1]))
+    mse = mse_line(X, T, W)
+    print("SD={0:.3f} cm".format(np.sqrt(mse)))
+    plt.figure(figsize=(4, 4))
+    show_line(W)
+    plt.plot(X, T, marker='o', linestyle='None',
+             color='cornflowerblue', markeredgecolor='black')
+    plt.xlim(X_min, X_max)
+    plt.grid(True)
+    plt.show()
+
+
+
+print('X0[나이]= ', np.round(X0, 2))
+print('X1[몸무게]= ', np.round(X1, 2))
+print('T[키]= ', np.round(T, 2))
+
+
+# 리스트 5-1-(14)
+# 2차원 데이터의 표시 ------------------------
+def show_data2(ax, x0, x1, t):
+    for i in range(len(x0)):
+        ax.plot([x0[i], x0[i]], [x1[i], x1[i]],
+                [120, t[i]], color='gray')
+        ax.plot(x0, x1, t, 'o',
+                color='cornflowerblue', markeredgecolor='black',
+                markersize=6, markeredgewidth=0.5)
+        ax.view_init(elev=35, azim=-75)
+
+
+# 메인 ------------------------------------
+plt.figure(figsize=(6, 5))
+ax = plt.subplot(1,1,1,projection='3d')
+show_data2(ax, X0, X1, T)
+plt.show()
+
+
+
+# 리스트 5-1-(15)
+#면의 표시 ----------------------------------
+def show_plane(ax, w):
+    px0 = np.linspace(X0_min, X0_max, 5)
+    px1 = np.linspace(X1_min, X1_max, 5)
+    px0, px1 = np.meshgrid(px0, px1)
+    y = w[0]*px0 + w[1] * px1 + w[2]
+    ax.plot_surface(px0, px1, y, rstride=1, cstride=1, alpha=0.3,
+                    color='blue', edgecolor='black')
+
+#면의 MSE -----------------------------------
+def mse_plane(x0, x1, t, w):
+    y = w[0] * x0 + w[1] * x1 + w[2] # (A)
+    mse = np.mean((y - t)**2)
+    return mse
+
+# 메인 ------------------------------------
+plt.figure(figsize=(6, 5))
+ax = plt.subplot(1, 1, 1, projection='3d')
+W = [1.5, 1, 90]
+show_plane(ax, W)
+show_data2(ax, X0, X1, T)
+mse = mse_plane(X0, X1, T, W)
+print("SD={0:.3f} cm".format(np.sqrt(mse)))
+plt.show()
+
+
+# 리스트 5-1-(16)
+# 해석해 ------------------------------------
+def fit_plane(x0, x1, t):
+    c_tx0 = np.mean(t * x0) - np.mean(t) * np.mean(x0)
+    c_tx1 = np.mean(t * x1) - np.mean(t) * np.mean(x1)
+    c_x0x1 = np.mean(x0 * x1) - np.mean(x0) * np.mean(x1)
+    v_x0 = np.var(x0)
+    v_x1 = np.var(x1)
+    w0 = (c_tx1 * c_x0x1 - v_x1 * c_tx0) / (c_x0x1**2 - v_x0 * v_x1)
+    w1 = (c_tx0 * c_x0x1 - v_x0 * c_tx1) / (c_x0x1**2 - v_x0 * v_x1)
+    w2 = -w0 * np.mean(x0) - w1 * np.mean(x1) + np.mean(t)
+    return np.array([w0, w1, w2])
+
+
+# 메인 ------------------------------------
+plt.figure(figsize=(6, 5))
+ax = plt.subplot(1, 1, 1, projection='3d')
+W = fit_plane(X0, X1, T)
+print("w0={0:.1f}, w1={1:.1f}, w2={2:.1f}".format(W[0], W[1], W[2]))
+show_plane(ax, W)
+show_data2(ax, X0, X1, T)
+mse = mse_plane(X0, X1, T, W)
+print("SD={0:.3f} cm".format(np.sqrt(mse)))
+plt.show()
+
+
+# --- 리스트 5-2-(1)
+
+# 데이터 로드 ----------------------------
+outfile = np.load('ch5_data.npz')
+X = outfile['X']
+X_min = outfile['X_min']
+X_max = outfile['X_max']
+X_n = outfile['X_n']
+T = outfile['T']
+
+print('T= ', T)
+
+
+
+# --- 리스트 5-2-(2)
+# 가우스 함수 ---------------------------------
+def gauss(x, mu, s):
+    return np.exp(-(x - mu)**2 / (2 * s**2))
+
+
+# 리스트 5-2-(3)
+# 메인 ------------------------------------
+M = 4
+plt.figure(figsize=(4, 4))
+mu = np.linspace(5, 30, M)
+s = mu[1] - mu[0] # (A)
+xb = np.linspace(X_min, X_max, 100)
+for j in range(M):
+    y = gauss(xb, mu[j], s)
+    plt.plot(xb, y, color='gray', linewidth=3)
+plt.grid(True)
+plt.xlim(X_min, X_max)
+plt.ylim(0, 1.2)
+plt.show()
+
+
+# 리스트 5-2-(4)
+# 선형 기저 함수 모델 ----------------
+def gauss_func(w, x):
+    m = len(w) - 1
+    mu = np.linspace(5, 30, m)
+    s = mu[1] - mu[0]
+    y = np.zeros_like(x) # x와 같은 크기로 요소가 0의 행렬 y를 작성
+    for j in range(m):
+        y = y + w[j] * gauss(x, mu[j], s)
+    y = y + w[m]
+    return y
+
+
+
+# 리스트 5-2-(5)
+# 선형 기저 함수 모델 MSE ----------------
+def mse_gauss_func(x, t, w):
+    y = gauss_func(w, x)
+    mse = np.mean((y - t)**2)
+    return mse
+
+
+# 리스트 5-2-(6)
+# 선형 기저 함수 모델 정확한 솔루션 -----------------
+def fit_gauss_func(x, t, m):
+    mu = np.linspace(5, 30, m)
+    s = mu[1] - mu[0]
+    n = x.shape[0]
+    psi = np.ones((n, m + 1))
+    for j in range(m):
+        psi[:, j] = gauss(x, mu[j], s)
+    psi_T = np.transpose(psi)
+
+    b = np.linalg.inv(psi_T.dot(psi))
+    c = b.dot(psi_T)
+    w = c.dot(t)
+    return w
+
+
+# 리스트 5-2-(7)
+# 가우스 기저 함수 표시 -----------------------
+def show_gauss_func(w):
+    xb = np.linspace(X_min, X_max, 100)
+    y = gauss_func(w, xb)
+    plt.plot(xb, y, c=[.5, .5, .5], lw=4)
+
+
+# 메인 ----------------------------------
+plt.figure(figsize=(4, 4))
+M = 4
+W = fit_gauss_func(X, T, M)
+show_gauss_func(W)
+plt.plot(X, T, marker='o', linestyle='None',
+         color='cornflowerblue', markeredgecolor='black')
+plt.xlim(X_min, X_max)
+plt.grid(True)
+mse = mse_gauss_func(X, T, W)
+print('W='+ str(np.round(W,1)))
+print("SD={0:.2f} cm".format(np.sqrt(mse)))
+plt.show()
+
+# 리스트 5-2-(8)
+plt.figure(figsize=(10, 2.5))
+plt.subplots_adjust(wspace=0.3)
+M = [2, 4, 7, 9]
+for i in range(len(M)):
+    plt.subplot(1, len(M), i + 1)
+    W = fit_gauss_func(X, T, M[i])
+    show_gauss_func(W)
+    plt.plot(X, T, marker='o', linestyle='None',
+             color='cornflowerblue', markeredgecolor='black')
+    plt.xlim(X_min, X_max)
+    plt.grid(True)
+    plt.ylim(130, 180)
+    mse = mse_gauss_func(X, T, W)
+
+    plt.title("M={0:d}, SD={1:.1f}".format(M[i], np.sqrt(mse)))
+plt.show()
+
+
+
+# 리스트 5-2-(10)
+# 훈련 데이터와 테스트 데이터 ------------------
+X_test = X[:int(X_n / 4 + 1)]
+T_test = T[:int(X_n / 4 + 1)]
+X_train = X[int(X_n / 4 + 1):]
+T_train = T[int(X_n / 4 + 1):]
+# 메인 ------------------------------------
+plt.figure(figsize=(10, 2.5))
+
+
+plt.subplots_adjust(wspace=0.3)
+M = [2, 4, 7, 9]
+for i in range(len(M)):
+    plt.subplot(1, len(M), i + 1)
+    W = fit_gauss_func(X_train, T_train, M[i])
+    show_gauss_func(W)
+    plt.plot(X_train, T_train, marker='o',
+             linestyle='None', color='white',
+             markeredgecolor='black', label='training')
+    plt.plot(X_test, T_test, marker='o', linestyle='None',
+             color='cornflowerblue',
+             markeredgecolor='black', label='test')
+    plt.legend(loc='lower right', fontsize=10, numpoints=1)
+    plt.xlim(X_min, X_max)
+    plt.ylim(130, 180)
+    plt.grid(True)
+    mse = mse_gauss_func(X_test, T_test, W)
+    plt.title("M={0:d}, SD={1:.1f}".format(M[i], np.sqrt(mse)))
+plt.show()
+
+# 리스트 5-2-(11)
+plt.figure(figsize=(5, 4))
+M = range(2, 10)
+mse_train = np.zeros(len(M))
+mse_test = np.zeros(len(M))
+for i in range(len(M)):
+    W = fit_gauss_func(X_train, T_train, M[i])
+    mse_train[i] = np.sqrt(mse_gauss_func(X_train, T_train, W))
+    mse_test[i] = np.sqrt(mse_gauss_func(X_test, T_test, W))
+plt.plot(M, mse_train, marker='o', linestyle='-',
+         markerfacecolor='white', markeredgecolor='black',
+         color='black', label='training')
+plt.plot(M, mse_test, marker='o', linestyle='-',
+         color='cornflowerblue', markeredgecolor='black',
+         label='test')
+plt.legend(loc='upper left', fontsize=10)
+plt.ylim(0, 12)
+plt.grid(True)
+plt.show()
