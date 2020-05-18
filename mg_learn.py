d39d1aaa2d981a7af5cd68c45d71ea2b28ca3377
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-05-18 10:18:51 +0900

    Update mg_learn.py

--- mg_learn.py
+++ mg_learn.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mglearn
-# matplotlib inline
+# %matplotlib inline
 
 plt.figure(1)
 mglearn.plots.plot_knn_classification(n_neighbors=1)
 
 plt.figure(2)
 mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+
+
 
 
