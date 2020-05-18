d5d2a6396e5ebc8b552c4f8830f547318baded94
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-05-18 10:12:23 +0900

    Create mg_learn.py

---
+++ mg_learn.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+# matplotlib inline
+
+plt.figure(1)
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+
+plt.figure(2)
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+
+
