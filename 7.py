985041e87dc3c1c821d2ac09ef093ba1d3b7127b
SCKIMOSU <sckim7@kookmin.ac.kr>
2020-06-17 13:18:25 +0900

    Update 7.py

--- 7.py
+++ 7.py
@@ -345,6 +345,211 @@ Show_data(X_test, T_test)
 show_FNN(WV, M, K)
 plt.show()
 
+
+# 리스트 7-1-(11)
+# -- 해석적 미분 -----------------------------------
+def dCE_FNN(wv, M, K, x, t):
+    # wv=WV; M; K; x=X_train[:N, :];  t=T_train[:N, :]
+
+    N, D = x.shape
+    # N, D
+    # =(2, 2)
+    # wv을 w와 v로 되돌림
+    w = wv[:M * (D + 1)]
+    # np.round(wv, 2)
+    # array([ 1.62, -0.61, -0.53, -1.07,  0.87, -2.3 ,  1.74, -0.76,  0.32,
+    #        -0.25,  1.46, -2.06, -0.32, -0.38,  1.13])
+    # np.round(w, 2)
+    # array([ 1.62, -0.61, -0.53, -1.07,  0.87, -2.3 ])
+
+    w = w.reshape(M, (D + 1))
+    # np.round(w, 2)
+    # array([[ 1.62, -0.61, -0.53],
+    #        [-1.07,  0.87, -2.3 ]])
+    v = wv[M * (D + 1):]
+    # np.round(v, 2)
+    # array([ 1.74, -0.76,  0.32, -0.25,  1.46, -2.06, -0.32, -0.38,  1.13])
+
+    v = v.reshape((K, M + 1))
+    # np.round(v, 2)
+    # array([[ 1.74, -0.76,  0.32],
+    #        [-0.25,  1.46, -2.06],
+    #        [-0.32, -0.38,  1.13]])
+
+    # ① x를 입력하여 y를 얻음
+    y, a, z, b = FNN(wv, M, K, x)
+    # np.round(y, 2)
+    # array([[0.38, 0.04, 0.59],
+    #        [0.4 , 0.03, 0.57]])
+    # np.round(a, 2)
+    # array([[ 0.55, -1.82,  0.99],
+    #        [ 0.67, -2.  ,  1.03]])
+    # np.round(z, 2)
+    # array([[0.22, 0.2 , 1.  ],
+    #        [0.24, 0.08, 1.  ]])
+    # np.round(b, 2)
+    # array([[-1.29, -1.4 ,  0.  ],
+    #        [-1.18, -2.45,  0.  ]])
+
+    # 출력 변수의 준비
+    dwv = np.zeros_like(wv)
+    # dwv
+    # array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+    dw = np.zeros((M, D + 1))
+    # dw
+    # array([[0., 0., 0.],
+    #        [0., 0., 0.]])
+
+    dv = np.zeros((K, M + 1))
+    # dv
+    # array([[0., 0., 0.],
+    #        [0., 0., 0.],
+    #        [0., 0., 0.]])
+
+    delta1 = np.zeros(M)  # 1층 오차
+    # delta1
+    # array([0., 0.])
+
+    delta2 = np.zeros(K)  # 2층 오차(k = 0 부분은 사용하지 않음)
+    # delta2
+    # array([0., 0., 0.])
+
+    for n in range(N):  # (A)
+        # ② 출력층의 오차를 구하기
+        for k in range(K):
+            delta2[k] = (y[n, k] - t[n, k])
+            # np.round(y,2)
+            # array([[0.38, 0.04, 0.59],
+            #        [0.4 , 0.03, 0.57]])
+            # np.round(t,2)
+            # array([[0, 1, 0],
+            #        [1, 0, 0]], dtype=uint8)
+            # np.round(delta2,2)
+            # array([ 0.38, -0.96,  0.59])
+
+            # k=0
+            # y[n, k] - t[n, k]
+            # 0.377
+            # y[n, k]
+            # 0.377
+            # t[n, k]
+            # 0
+
+            # k=1
+            # y[n, k] - t[n, k]
+            # -0.965
+            # y[n, k]
+            # 0.035
+            # t[n, k]
+            # 1
+
+
+        # ③ 중간층의 오차를 구하기
+        for j in range(M):
+            delta1[j] = z[n, j] * (1 - z[n, j]) * np.dot(v[:, j], delta2)
+        # ④ v의 기울기 dv를 구하기
+        for k in range(K):
+            dv[k, :] = dv[k, :] + delta2[k] * z[n, :] / N
+        # ④ w의 기울기 dw를 구하기
+        for j in range(M):
+            dw[j, :] = dw[j, :] + delta1[j] * np.r_[x[n, :], 1] / N
+    # dw와 dv를 합체시킨 dwv로 만들기
+    dwv = np.c_[dw.reshape((1, M * (D + 1))), \
+                dv.reshape((1, K * (M + 1)))]
+    dwv = dwv.reshape(-1)
+    return dwv
+
+
+# ------Show VW
+def Show_dWV(wv, M):
+    N = wv.shape[0]
+    plt.bar(range(1, M * 3 + 1), wv[:M * 3],
+            align="center", color='black')
+    plt.bar(range(M * 3 + 1, N + 1), wv[M * 3:],
+            align="center", color='cornflowerblue')
+    plt.xticks(range(1, N + 1))
+    plt.xlim(0, N + 1)
+
+
+# -- 동작 확인
+M = 2
+K = 3
+N = 2
+nWV = M * 3 + K * (M + 1)
+np.random.seed(1)
+WV = np.random.normal(0, 1, nWV)
+
+dWV_ana = dCE_FNN(WV, M, K, X_train[:N, :], T_train[:N, :])
+print("analytical dWV")
+print(dWV_ana)
+
+dWV_num = dCE_FNN_num(WV, M, K, X_train[:N, :], T_train[:N, :])
+print("numerical dWV")
+print(dWV_num)
+
+plt.figure(1, figsize=(8, 3))
+plt.subplots_adjust(wspace=0.5)
+plt.subplot(1, 2, 1)
+Show_dWV(dWV_ana, M)
+plt.title('analitical')
+plt.subplot(1, 2, 2)
+Show_dWV(dWV_num, M)
+plt.title('numerical')
+plt.show()
+
+
+# 리스트 7-1-(12)
+import time
+
+
+# 해석적 미분을 사용한 구배법 -------
+def Fit_FNN(wv_init, M, K, x_train, t_train, x_test, t_test, n, alpha):
+    wv = wv_init.copy()
+    err_train = np.zeros(n)
+    err_test = np.zeros(n)
+    wv_hist = np.zeros((n, len(wv_init)))
+    epsilon = 0.001
+    for i in range(n):
+        wv = wv - alpha * dCE_FNN(wv, M, K, x_train, t_train) # (A)
+        err_train[i] = CE_FNN(wv, M, K, x_train, t_train)
+        err_test[i] = CE_FNN(wv, M, K, x_test, t_test)
+        wv_hist[i, :] = wv
+    return wv, wv_hist, err_train, err_test
+
+
+# 메인 ---------------------------
+startTime = time.time()
+M = 2
+K = 3
+np.random.seed(1)
+WV_init = np.random.normal(0, 0.01, M * 3 + K * (M + 1))
+N_step = 1000
+alpha = 1
+WV, WV_hist, Err_train, Err_test = Fit_FNN(
+    WV_init, M, K, X_train, T_train, X_test, T_test, N_step, alpha)
+calculation_time = time.time() - startTime
+print("Calculation time:{0:.3f} sec".format(calculation_time))
+
+# 리스트 7-1-(13)
+plt.figure(1, figsize=(12, 3))
+plt.subplots_adjust(wspace=0.5)
+# 학습 오차의 표시 ---------------------------
+plt.subplot(1, 3, 1)
+plt.plot(Err_train, 'black', label='training')
+plt.plot(Err_test, 'cornflowerblue', label='test')
+plt.legend()
+# 가중치의 시간 변화 표시 ---------------------------
+plt.subplot(1, 3, 2)
+plt.plot(WV_hist[:, :M * 3], 'black')
+plt.plot(WV_hist[:, M * 3:], 'cornflowerblue')
+# 경계선 표시 --------------------------
+plt.subplot(1, 3, 3)
+Show_data(X_test, T_test)
+M = 2
+K = 3
+show_FNN(WV, M, K)
+plt.show()
+
 # 리스트 7-1-(14)
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -388,3 +593,109 @@ for k in range(K):
     show_activation3d(ax, y[:, k], [0, 1], '$y_{0:d}$'.format(k))
 
 plt.show()
+
+
+# 리스트 7-2-(1)
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+np.random.seed(1) # (A)
+import keras.optimizers # (B)
+from keras.models import Sequential # (C)
+from keras.layers.core import Dense, Activation #(D)
+
+
+# 데이터 로드 ---------------------------
+outfile = np.load('class_data.npz')
+X_train = outfile['X_train']
+T_train = outfile['T_train']
+X_test = outfile['X_test']
+T_test = outfile['T_test']
+X_range0 = outfile['X_range0']
+X_range1 = outfile['X_range1']
+
+
+# 리스트 7-2-(2)
+# 데이터를 그리기 ------------------------------
+def Show_data(x, t):
+    wk, n = t.shape
+    c = [[0, 0, 0], [.5, .5, .5], [1, 1, 1]]
+    for i in range(n):
+        plt.plot(x[t[:, i] == 1, 0], x[t[:, i] == 1, 1],
+                 linestyle='none', marker='o',
+                 markeredgecolor='black',
+                 color=c[i], alpha=0.8)
+    plt.grid(True)
+
+
+# 리스트 7-2-(3)
+# 난수 초기화
+np.random.seed(1)
+
+
+# --- Sequential 모델 작성
+model = Sequential()
+model.add(Dense(2, input_dim=2, activation='sigmoid',
+                kernel_initializer='uniform')) # (A)
+model.add(Dense(3,activation='softmax',
+                kernel_initializer='uniform')) # (B)
+sgd = keras.optimizers.SGD(lr=1, momentum=0.0,
+                           decay=0.0, nesterov=False) # (C)
+model.compile(optimizer=sgd, loss='categorical_crossentropy',
+              metrics=['accuracy']) # (D)
+
+
+# ---------- 학습
+startTime = time.time()
+history = model.fit(X_train, T_train, epochs=1000, batch_size=100,
+                    verbose=0, validation_data=(X_test, T_test)) # (E)
+
+
+# ---------- 모델 평가
+score = model.evaluate(X_test, T_test, verbose=0) # (F)
+print('cross entropy {0:3.2f}, accuracy {1:3.2f}'\
+      .format(score[0], score[1]))
+calculation_time = time.time() - startTime
+print("Calculation time:{0:.3f} sec".format(calculation_time))
+
+
+# 리스트 7-2-(4)
+plt.figure(1, figsize = (12, 3))
+plt.subplots_adjust(wspace=0.5)
+
+
+# 학습 곡선 표시 --------------------------
+plt.subplot(1, 3, 1)
+plt.plot(history.history['loss'], 'black', label='training') # (A)
+plt.plot(history.history['val_loss'], 'cornflowerblue', label='test') # (B)
+plt.legend()
+
+
+# 정확도 표시 --------------------------
+plt.subplot(1, 3, 2)
+plt.plot(history.history['acc'], 'black', label='training') # (C)
+plt.plot(history.history['val_acc'], 'cornflowerblue', label='test') # (D)
+plt.legend()
+
+
+# 경계선 표시 --------------------------
+plt.subplot(1, 3, 3)
+Show_data(X_test, T_test)
+xn = 60 # 등고선 표시 해상도
+x0 = np.linspace(X_range0[0], X_range0[1], xn)
+x1 = np.linspace(X_range1[0], X_range1[1], xn)
+xx0, xx1 = np.meshgrid(x0, x1)
+x = np.c_[np.reshape(xx0, xn * xn, 1), np.reshape(xx1, xn * xn, 1)]
+y = model.predict(x) # (E)
+K = 3
+for ic in range(K):
+    f = y[:, ic]
+    f = f.reshape(xn, xn)
+    f = f.T
+    cont = plt.contour(xx0, xx1, f, levels=[0.5, 0.9], colors=[
+        'cornflowerblue', 'black'])
+    cont.clabel(fmt='%1.1f', fontsize=9)
+    plt.xlim(X_range0)
+    plt.ylim(X_range1)
+plt.show()
+
