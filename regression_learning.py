5e2ca367d421d7d0678f33b17496c92443034ae5
SCKIMOSU <sckim7@kookmin.ac.kr>
2021-04-26 17:29:31 +0900

    Update regression_learning.py

--- regression_learning.py
+++ regression_learning.py
@@ -1,8 +1,15 @@
 
 
+
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
+#import tensorflow as tf
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+# 텐서플로우 2.0 환경에서 1.x 코드 실행하기
+print(tf.__version__)
+
 
 def Data_Genearion(num_points):
     #num_points = 50
